e5b61fd65555b7aa910669e65dbd86490e63fe28
vijupatil1004-beep <vijupatil1004@gmail.com>
2026-01-16 12:35:00 +0530

    Store extracted Salesforce data with deduplication

--- public/src/background/serviceWorker.js
+++ public/src/background/serviceWorker.js
@@ -1,7 +1,19 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
- if (message.action === "SAVE_DATA") {
+ if (message.action === "SAVE_RECORD") {
    chrome.storage.local.get("salesforce_data", (res) => {
-     const data = res.salesforce_data || {};
+     const data = res.salesforce_data || {
+       opportunities: [],
+       lastSync: Date.now()
+     };
+     const list = data[message.objectType] || [];
+     const index = list.findIndex(r => r.id === message.record.id);
+     if (index > -1) {
+       list[index] = message.record;
+     } else {
+       list.push(message.record);
+     }
+     data[message.objectType] = list;
+     data.lastSync = Date.now();
      chrome.storage.local.set({ salesforce_data: data });
    });
  }
