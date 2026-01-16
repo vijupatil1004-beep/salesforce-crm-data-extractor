4f36a9d71f924da253875ea3db97ff35ca4b20cf
vijupatil1004-beep <vijupatil1004@gmail.com>
2026-01-16 12:29:10 +0530

    Show extraction status using Shadow DOM

--- public/src/content/index.js
+++ public/src/content/index.js
@@ -1,1 +1,9 @@
 console.log("Salesforce CRM Data Extractor loaded");
+import { detectObjectType } from "./objectDetector";
+import { showStatus } from "./shadowStatus";
+const objectType = detectObjectType();
+if (objectType) {
+ showStatus(`Detected Salesforce ${objectType}`, "success");
+} else {
+ showStatus("Salesforce object not detected", "error");
+}
